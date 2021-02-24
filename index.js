0e6f0f7c892eaf322b0d2547055abba1ef641259
rafi-windows <rafi_n_o@github.com>
2021-02-24 21:29:17 +0700

    restruktur project

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
 const express = require("express");
 
 const app = express();
-const router = express.Router();
+const productRoutes = require("./src/routes/products");
 
-router.use("/products", (req, res, next) => {
-  res.json({ name: "Rafi Nur Oktafian", email: "rafi@gmail.com" });
-  next();
-});
-
-router.use("/price", (req, res, next) => {
-  res.json({ price: 30000000 });
-  next();
-});
-
-router.delete("/customers", (req, res, next) => {
-  res.json({ title: "Customer" });
-  next();
-});
-
-app.use("/", router);
-
-// GET '/users' => [{name: rafi}]
+app.use("/", productRoutes);
 
 app.listen(4000);

2fc8291d13665ead5d21c4cc6639243cf5f75619
rafi-windows <rafi_n_o@github.com>
2021-03-17 13:13:36 +0700

    get post by id

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -52,3 +52,23 @@ exports.getAllBlogPost = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getBlogPostById = (req, res, next) => {
+  const postId = req.params.postId;
+  BlogPost.findById(postId)
+    .then((result) => {
+      if (!result) {
+        const error = new Error("Blog Post tidak ditemukan");
+        error.errorStatus = 404;
+        throw error;
+      }
+
+      res.status(200).json({
+        message: "Data Blog Post Berhasil dipanggil",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
